bc1ffbb437e9535990d06fb38d8f1967daca86e3
Nathanlobo10 <74343482+Nathanlobo10@users.noreply.github.com>
2020-12-23 20:47:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Project-cars
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50); 
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+
+//tr i wasnt abble to do this project i will comeplete it cause of christmass
